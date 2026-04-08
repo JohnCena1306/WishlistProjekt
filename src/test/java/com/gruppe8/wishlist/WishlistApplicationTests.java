f357f72b33d16714d6623830ea6beb0cfd89a66d
JohnCena1306 <oliverjoostpersson@icloud.com>
2026-04-08 11:12:25 +0200

    ja

---
+++ src/test/java/com/gruppe8/wishlist/WishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruppe8.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
