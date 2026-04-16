529ac1868f9e0761fa25bb2058d442ac2d67c99f
JohnCena1306 <oliverjoostpersson@icloud.com>
2026-04-16 15:03:41 +0200

    SQL connection dependencies og application properties, ItemRepository CRUD metoder, ItemRowMapper ændret schema pga den crashede (date defeault virkede ik)

--- src/main/java/com/gruppe8/wishlist/model/Item.java
+++ src/main/java/com/gruppe8/wishlist/model/Item.java
@@ -2,16 +2,27 @@ package com.gruppe8.wishlist.model;
 
 public class Item {
 
+    private int id;
     private String name;
     private String description;
     private double price;
     private String link;
 
-    public Item(String name, String description, double price, String link){
+
+    public Item(int id, String name, String description, double price, String link){
+        this.id = id;
         this.name = name;
         this.description = description;
         this.price = price;
         this.link = link;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
