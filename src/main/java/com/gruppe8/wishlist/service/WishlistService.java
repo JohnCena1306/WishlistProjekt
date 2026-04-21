0bf17f0b94807c0ea3c9b3a0230f28d4ea777fb9
MarkusLarsen0410 <Markuslarsen0410@hotmail.com>
2026-04-21 12:26:35 +0200

    Funktion lavet således at man kan se ønskelistens indhold

--- src/main/java/com/gruppe8/wishlist/service/WishlistService.java
+++ src/main/java/com/gruppe8/wishlist/service/WishlistService.java
@@ -1,30 +1,33 @@
 package com.gruppe8.wishlist.service;
 
 import com.gruppe8.wishlist.model.WishList;
+import com.gruppe8.wishlist.repository.ItemRepository;
 import com.gruppe8.wishlist.repository.WishlistRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WishlistService {
 
+    private final ItemRepository itemRepository;
     private WishlistRepository wishlistRepository;
 
 
-    public  WishlistService(WishlistRepository wishlistRepository) {
+    public WishlistService(WishlistRepository wishlistRepository, ItemRepository itemRepository) {
         this.wishlistRepository = wishlistRepository;
+        this.itemRepository = itemRepository;
     }
 
-    public void saveWishList(WishList wishList){
+    public void saveWishList(WishList wishList) {
 
         wishlistRepository.addWishlist(wishList);
-
     }
 
-    public void deleteWishListByTitle(int id){
 
-        wishlistRepository.deleteWishListByTitle(id);
+    public void deleteWishListByTitle(int id) {
+
+        itemRepository.deleteByWishListId(id);
+        wishlistRepository.deleteWishList(id);
     }
-
 
 
 }
